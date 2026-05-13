3c3f12955f0e02c13f9eaef0b78d5cb11b6a8a10
kwaamfan <balfiere@proton.me>
2026-05-13 16:06:58 -0500

    use relative links to js libraries

--- src/assets/js/gallery-lightbox.js
+++ src/assets/js/gallery-lightbox.js
@@ -1,5 +1,5 @@
-import PhotoSwipeLightbox from "/assets/js/vendor/photoswipe-lightbox.esm.js";
-import PhotoSwipeDynamicCaption from "/assets/js/vendor/photoswipe-dynamic-caption-plugin.esm.js";
+import PhotoSwipeLightbox from "./vendor/photoswipe-lightbox.esm.js";
+import PhotoSwipeDynamicCaption from "./vendor/photoswipe-dynamic-caption-plugin.esm.js";
 
 const galleryEl = document.getElementById("gallery");
 if (galleryEl) galleryEl.classList.add("js-enabled");
@@ -7,7 +7,7 @@ if (galleryEl) galleryEl.classList.add("js-enabled");
 const lightbox = new PhotoSwipeLightbox({
   gallery: "#gallery",
   children: ".gallery-item:not([hidden]) a.photoswipe",
-  pswpModule: () => import("/assets/js/vendor/photoswipe.esm.js"),
+  pswpModule: () => import("./vendor/photoswipe.esm.js"),
 });
 
 const _captionPlugin = new PhotoSwipeDynamicCaption(lightbox, {
